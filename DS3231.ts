49c4c09e1fcb59c5ec2f0a8e1a8200e8017680d7
Александр Фролов <frolov@itmatrix.ru>
2020-07-24 16:12:20 +0300

    Update DS3231.ts

--- DS3231.ts
+++ DS3231.ts
@@ -14,4 +14,59 @@ namespace DS3231 {
 
     initialize()
 
+    function leadingZero(value: number): string {
+        if (value < 10) {
+            return "0" + value
+        }
+        return "" + value
+    }
+
+    function getRegister(register: number): number {
+        let data = pins.createBuffer(1)
+        data[0] = register
+        pins.i2cWriteBuffer(I2C_ADDR, data)
+        return pins.i2cReadNumber(I2C_ADDR, NumberFormat.Int8LE)
+    }
+
+    function setRegister(register: number, value: number) {
+        let data = pins.createBuffer(2)
+        data[0] = register
+        data[1] = value
+        pins.i2cWriteBuffer(I2C_ADDR, data)
+    }
+
+    export function getTime(): number[] {
+        let hour = bcd.Decode(getRegister(REG_HOUR))
+        let mins = bcd.Decode(getRegister(REG_MINS))
+        let secs = bcd.Decode(getRegister(REG_SECS))
+        return [hour, mins, secs]
+    }
+
+    export function setTime(hour: number, mins: number, secs: number) {
+        setRegister(REG_HOUR, bcd.Encode(hour))
+        setRegister(REG_MINS, bcd.Encode(mins))
+        setRegister(REG_SECS, bcd.Encode(secs))
+    }
+
+    export function setTimeString(input: string) {
+        let time = helpers.stringSplit(input, ":")
+
+        let hour = parseInt(time[0]) % 24;
+        let mins = parseInt(time[1]) % 60;
+        let secs = parseInt(time[2]) % 60;
+
+        setTime(hour, mins, secs)
+    }
+
+    export function getTimeString(): string {
+        let time = getTime()
+
+        let hour = leadingZero(time[0])
+        let mins = leadingZero(time[1])
+        let secs = leadingZero(time[2])
+
+        return `${hour}:${mins}:${secs}`
+    }
+
+
 }
