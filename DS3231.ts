44aef5f631132cd37844cbbfffb1b9dd2f038409
Александр Фролов <frolov@itmatrix.ru>
2020-07-26 12:40:24 +0300

    Update DS3231.ts

--- DS3231.ts
+++ DS3231.ts
@@ -241,7 +241,7 @@ namespace DS3231 {
      * DS3231 Control
      */
     //% blockId="DS3231_CONTROL" block="status"
-    export function Status(): number {
+    export function Control(): number {
 //        let ctrl = bcd.Decode(getRegister(DS3231_CONTROL_ADDR))
         let ctrl = getRegister(DS3231_CONTROL_ADDR)
         return ctrl
