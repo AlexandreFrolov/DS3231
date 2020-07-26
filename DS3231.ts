7a2dd93c789f0163c958d55c28f19580491b90bf
Александр Фролов <frolov@itmatrix.ru>
2020-07-26 10:59:50 +0300

    Update DS3231.ts

--- DS3231.ts
+++ DS3231.ts
@@ -48,10 +48,18 @@ namespace DS3231 {
      * DecToHexString
      */
     //% block
-    export function DecToHexString(dec_value: number): string {
-        return "dec_value"
+    export function DecToHexString(int: number, base: number): string {
+        let letters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        let returnVal = "";
+        if (base > 1 && base < 37) {
+            while (int != 0) {
+                let rest = int % base;
+                int = Math.floor(int / base);
+                returnVal = letters[rest] + returnVal;
+            }
+        }
+        return returnVal;
     }
-
 
 
     /**
