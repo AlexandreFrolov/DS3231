d6b1af75481e0c14d7537ae444e67e6b2dac6cfd
Александр Фролов <frolov@itmatrix.ru>
2020-07-26 10:27:31 +0300

    Update DS3231.ts

--- DS3231.ts
+++ DS3231.ts
@@ -49,7 +49,7 @@ namespace DS3231 {
      */
     //% block
     export function DecToHexText(dec_value: number): string {
-        return dec_value.toString()
+        return dec_value.toString(16)
     }
 
 
