80068d929e01b4d563bd57fc1f077191128bc173
Александр Фролов <frolov@itmatrix.ru>
2020-07-27 16:25:17 +0300

    Update DS3231.ts

--- DS3231.ts
+++ DS3231.ts
@@ -167,6 +167,7 @@ namespace DS3231 {
      */
     //% block="set date:|weekday $weekday day $day month $month year $year"
     //% weight=80
+    //% inlineInputMode=inline
     //% weekday.min=0 weekday.max=7 day.min=0 day.max=31 month.min=0 month.max=12 year.min=2000 year.max=2100
     export function setDate(weekday: number, day: number, month: number, year: number) {
         if(weekday >= 0 && weekday <= 7 && day >= 1 && day <= 31 && month >= 1 && month <= 12 && year >= 2000 && year < 2100) {
