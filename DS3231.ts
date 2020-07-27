4dbd7dc0776c87f1ed533a9cff9c89cf737c06c2
Александр Фролов <frolov@itmatrix.ru>
2020-07-27 11:19:07 +0300

    Update DS3231.ts

--- DS3231.ts
+++ DS3231.ts
@@ -2,7 +2,7 @@
 /**
  * DS3231 block
  */
-//% weight=100 color=#33ff33 icon="\uf042" block="DS3231 RTC"
+//% weight=100 color=#00cc00 icon="\uf042" block="DS3231 RTC"
 namespace DS3231 {
 
     const DS3231_I2C_ADDR=0x68
