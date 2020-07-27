15ffb13f17647e3c69092a73f51690f79d5c17b2
Александр Фролов <frolov@itmatrix.ru>
2020-07-27 11:48:13 +0300

    Update DS3231.ts

--- DS3231.ts
+++ DS3231.ts
@@ -227,7 +227,7 @@ namespace DS3231 {
     /**
      * setTime
      */
-    //% block="set DS3231 time"
+    //% block="set DS3231 time |hour: $hour mins: $mins secs: $secs"
     //% weight=3
     export function setTime(hour: number, mins: number, secs: number) {
         setRegister(DS3231_HOURS, Encode(hour))
