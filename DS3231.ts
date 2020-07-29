4dd2d93a60199ee61afcc95ff65bad44a3cb6713
Александр Фролов <frolov@itmatrix.ru>
2020-07-29 14:06:56 +0300

    Fix names

--- DS3231.ts
+++ DS3231.ts
@@ -93,7 +93,7 @@ namespace DS3231 {
     /**
      * setTime
      */
-    //% block="set time:|hour: $hour mins: $mins secs: $secs"
+    //% block="time:|hour: $hour mins: $mins secs: $secs"
     //% weight=100
     //% hour.min=0 hour.max=23 mins.min=0 mins.max=59 secs.min=0 secs.max=59
     export function setTime(hour: number, mins: number, secs: number) {
@@ -107,7 +107,7 @@ namespace DS3231 {
     /**
      * setDate
      */
-    //% block="set date:|weekday $weekday day $day month $month year $year"
+    //% block="date:|weekday $weekday day $day month $month year $year"
     //% weight=90
     //% inlineInputMode=inline
     //% weekday.min=0 weekday.max=7 day.min=0 day.max=31 month.min=0 month.max=12 year.min=2000 year.max=2100
