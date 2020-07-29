930b1ce334eaeb7df1c73a7477fabbe05c0ff49f
Александр Фролов <frolov@itmatrix.ru>
2020-07-29 10:21:51 +0300

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,6 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+DS3231.setDate(3, 29, 7, 2020)
+DS3231.setTime(10, 19, 0)
+basic.forever(function () {
+    basic.showNumber(DS3231.seconds() % 10)
+    basic.pause(100)
+})
